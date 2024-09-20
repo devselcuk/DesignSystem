6e8c1e565efc27ec87df8b2e6e3084b18f84b75f
Selcuk <devselcukdev@gmail.com>
2024-09-20 20:07:10 +0200

    feat: add black filled rounded button

---
+++ Sources/DesignSystem/BlackFilledRoundedButtonStyle.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  DesignSystem
+//
+//  Created by SELCUK YILDIZ on 20.09.24.
+//
+
+import SwiftUI
+
+public struct BlackFilledRoundedButtonStyle: ButtonStyle {
+    
+    public init(){}
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .frame(maxWidth: .infinity)
+            .bold()
+            .background(content: {
+                Color(.label)
+            })
+            .foregroundStyle(.background)
+            .clipShape(Capsule())
+            .opacity(configuration.isPressed ? 0.2 : 1)
+            .labelStyle(CenteredLabelStyle(spacing: 16))
+    }
+}
